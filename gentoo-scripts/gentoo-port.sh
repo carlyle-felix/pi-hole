46347290750e39fb9356bfb7336636c495a3c617
carlyle-felix <carlyle.felix@gmail.com>
2025-09-17 19:59:24 +0200

    gentoo-scripts: add support for removing pihole-meta overlay

--- gentoo-scripts/gentoo-port.sh
+++ gentoo-scripts/gentoo-port.sh
@@ -15,15 +15,23 @@ PIHOLE_META_PACKAGE_CONTROL_PORTAGE="pihole-meta"
 gentoo_package_management() {
 	local arg="$1"
 
+	# Generate meta package repo
 	if [[ "${arg}" == "generate_repo" ]]; then
-		# Generate meta package repo
 		gentoo_generate_repo
+
+	# set package manager variables
 	elif [[ "${arg}" == "set_package_manager" ]]; then
-		# set package manager variables
 		PKG_MANAGER="emerge"
 		PKG_INSTALL="${PKG_MANAGER} -avn"
 		PKG_REMOVE="${PKG_MANAGER} --deselect"
+
+	# Install the meta package
 	elif [[ "${arg}" == "install_meta" ]]; then
 		eval "$PKG_INSTALL" "${PIHOLE_META_PACKAGE_CONTROL_PORTAGE}"
+
+	# Remove the overlay when uninstalling pihole
+	elif [[ "${arg}" == "remove_overlay" ]]; then
+		${SUDO} rm -rf /var/db/repos/pihole-overlay &> /dev/null
+		${SUDO} rm /etc/portage/repos.conf/pihole.conf
 	fi
 }
